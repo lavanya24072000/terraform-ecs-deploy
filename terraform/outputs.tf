e659395fd166cbc5e5f1fa11985faa4289389e4d
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-02 18:53:02 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-  value = modules.vpc.vpc_id
+  value = module.vpc.vpc_id
 }
 
 output "alb_dns" {
-  value = modules.alb.alb_dns
+  value = module.alb.alb_dns
 }
 
 output "ecs_cluster_id" {
-  value = modules.ecs.ecs_cluster_id
+  value = module.ecs.ecs_cluster_id
 }
