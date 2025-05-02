7bc9afb89a5d5c37b468bc60f075a9fb923b10c2
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-02 21:36:31 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,3 +9,38 @@ resource "aws_subnet" "public" {
   availability_zone = element(var.azs, count.index)
   map_public_ip_on_launch = true
 }
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "main-vpc"
+  }
+}
+ 
+resource "aws_internet_gateway" "gw" {
+vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+ 
+
+ 
+ 
+resource "aws_route_table" "public" {
+vpc_id = aws_vpc.main.id
+ 
+  route {
+    cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+  }
+ 
+  tags = {
+    Name = "public-rt"
+  }
+}
+ 
+resource "aws_route_table_association" "public" {
+  count          =  3
+  subnet_id      = aws_subnet.public[count.index].id
+route_table_id = aws_route_table.public.id
+}
