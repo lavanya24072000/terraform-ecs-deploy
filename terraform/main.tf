e03cc297973a41fd20838adecfd7a12c790169ff
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-05-02 18:29:19 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,8 @@ module "alb" {
   alb_name          = "${var.env}-alb"
   subnet_ids = module.vpc.public_subnet_ids
   vpc_id            = module.vpc.vpc_id
-   security_group_id = var.alb_sg_id
+  security_group_id = var.alb_sg_id
+  env               = var.env
 }
  
 module "ecs" {
@@ -21,5 +22,5 @@ module "ecs" {
   nginx_tg_arn      = module.alb.nginx_tg_arn
   tomcat_tg_arn     = module.alb.tomcat_tg_arn
   apache_tg_arn     = module.alb.apache_tg_arn
-  env               = var.env
+ 
 }
